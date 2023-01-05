490537338268f9017d4d9cf4393fb0f29a1689c4
bobbyta74 <bobbyta74@gmail.com>
2023-01-05 15:18:10 +0100

    Add regex validation to name and phone inputs

--- script.js
+++ script.js
@@ -3,33 +3,22 @@ const confpwd = document.querySelector("#confirmpass");
 const myspan = document.querySelector("span");
 const lebtn = document.querySelector("button");
 
-pwd.addEventListener("input", function () {
+function confirmcorrect() {
     if (pwd.value != confpwd.value) {
         confpwd.setCustomValidity("Password and confirmed password must be the same");
         pwd.setCustomValidity("Password and confirmed password must be the same");
-        pwd.classList.add("error");
-        confpwd.classList.add("error");
         myspan.textContent = "*Passwords do not match";
     } else {
         myspan.textContent = "";
-        pwd.classList.remove("error");
-        confpwd.classList.remove("error");
         pwd.setCustomValidity("");
         confpwd.setCustomValidity("");
     }
+}
+
+pwd.addEventListener("input", function () {
+    confirmcorrect()
 })
 
 confpwd.addEventListener("input", function () {
-    if (pwd.value != confpwd.value) {
-        confpwd.setCustomValidity("Password and confirmed password must be the same");
-        pwd.classList.add("error");
-        confpwd.classList.add("error");
-        myspan.textContent = "*Passwords do not match";
-    } else {
-        myspan.textContent = "";
-        pwd.classList.remove("error");
-        confpwd.classList.remove("error");
-        pwd.setCustomValidity("");
-        confpwd.setCustomValidity("");
-    }
+    confirmcorrect()
 })
